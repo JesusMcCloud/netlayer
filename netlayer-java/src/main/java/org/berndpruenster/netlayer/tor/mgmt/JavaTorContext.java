fc1103827d349c307f2c970a2dc96ecb30be591d
Bernd Prünster <mail@berndpruenster.org>
2017-01-23 18:16:39 +0100

    Code Dump

---
+++ netlayer-java/src/main/java/org/berndpruenster/netlayer/tor/mgmt/JavaTorContext.java
@@ -0,0 +1,136 @@
+/*
+Copyright (c) 2016, 2017 Bernd Prünster
+Copyright (c) 2014-2015 Microsoft Open Technologies, Inc.
+All Rights Reserved
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache 2 License for the specific language governing permissions and limitations under the License.
+*/
+
+package org.berndpruenster.netlayer.tor.mgmt;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JavaTorContext extends TorContext {
+
+  private static final String FILE_ARCHIVE     = "tor.tar.xz";
+  private static final String BINARY_TOR_MACOS = "tor.real";
+  private static final String BINARY_TOR_WIN   = "tor.exe";
+  private static final String BINARY_TOR_LNX   = "tor";
+  private static final String PATH_LNX         = "linux/";
+  private static final String PATH_LNX64       = PATH_LNX + "x64/";
+  private static final String PATH_LNX32       = PATH_LNX + "x86/";
+  private static final String PATH_MACOS       = "osx/";
+  private static final String PATH_MACOS64     = PATH_MACOS + "x64/";
+  private static final String PATH_WIN         = "windows/";
+  private static final String PATH_WIN32       = PATH_WIN + "x86/";
+  private static final String PATH_NATIVE      = "native/";
+
+  public JavaTorContext(final File workingDirectory) {
+    super(workingDirectory);
+  }
+
+  @Override
+  public WriteObserver generateWriteObserver(final File file) {
+    try {
+      return new JavaWatchObserver(file);
+    } catch (final IOException e) {
+      throw new RuntimeException("Could not create JavaWatchObserver", e);
+    }
+  }
+
+  @Override
+  protected InputStream getAssetOrResourceByName(final String fileName) throws IOException {
+    return getClass().getResourceAsStream("/" + fileName);
+  }
+
+  @Override
+  public String getProcessId() {
+    // This is a horrible hack. It seems like more JVMs will return the
+    // process's PID this way, but not guarantees.
+    final String processName = java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
+    return processName.split("@")[0];
+  }
+
+  @Override
+  public void installFiles() throws IOException, InterruptedException {
+    super.installFiles();
+    switch (OsData.getOsType()) {
+    case WIN:
+    case LNX32:
+    case LNX64:
+    case MACOS:
+      FileUtilities.extractContentFromArchive(getWorkingDirectory(),
+          getAssetOrResourceByName(getPathToTorExecutable() + FILE_ARCHIVE));
+      break;
+    default:
+      throw new RuntimeException("We don't support Tor on this OS yet");
+    }
+  }
+
+  @Override
+  protected String getPathToTorExecutable() {
+
+    switch (OsData.getOsType()) {
+    case WIN:
+      return PATH_NATIVE + PATH_WIN32; // We currently only support the
+    // x86 build but that should work
+    // everywhere
+    case MACOS:
+      return PATH_NATIVE + PATH_MACOS64; // I don't think there even is a x32
+    // build of Tor for MACOS, but could be
+    // wrong.
+    case LNX32:
+      return PATH_NATIVE + PATH_LNX32;
+    case LNX64:
+      return PATH_NATIVE + PATH_LNX64;
+    default:
+      throw new RuntimeException("We don't support Tor on this OS");
+    }
+  }
+
+  @Override
+  protected String getPathToRC() {
+    return getRCPath() + FILE_TORRC_NATIVE;
+  }
+
+  private String getRCPath() {
+    switch (OsData.getOsType()) {
+    case WIN:
+      return PATH_NATIVE + PATH_WIN; // We currently only support the
+    // x86 build but that should work
+    // everywhere
+    case MACOS:
+      return PATH_NATIVE + PATH_MACOS; // I don't think there even is a x32
+    // build of Tor for MACOS, but could be
+    // wrong.
+    case LNX32:
+    case LNX64:
+      return PATH_NATIVE + PATH_LNX;
+    default:
+      throw new RuntimeException("We don't support Tor on this OS");
+    }
+  }
+
+  @Override
+  protected String getTorExecutableFileName() {
+    switch (OsData.getOsType()) {
+    case LNX32:
+    case LNX64:
+      return BINARY_TOR_LNX;
+    case WIN:
+      return BINARY_TOR_WIN;
+    case MACOS:
+      return BINARY_TOR_MACOS;
+    default:
+      throw new RuntimeException("We don't support Tor on this OS");
+    }
+  }
+}
---
+++ netlayer/src/main/java/org/berndpruenster/netlayer/tor/mgmt/FileUtilities.java
@@ -0,0 +1,138 @@
+/*
+Copyright (c) 2016, 2017 Bernd Prünster
+Copyright (c) 2014-2015 Microsoft Open Technologies, Inc.
+All Rights Reserved
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache 2 License for the specific language governing permissions and limitations under the License.
+*/
+
+package org.berndpruenster.netlayer.tor.mgmt;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
+import org.apache.commons.compress.utils.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileUtilities {
+  private static final Logger LOG = LoggerFactory.getLogger(FileUtilities.class);
+
+  private FileUtilities() {
+  }
+
+  static void logFiles(final File f) {
+    if (f.isDirectory()) {
+      for (final File child : f.listFiles()) {
+        logFiles(child);
+      }
+    } else {
+      LOG.info(f.getAbsolutePath());
+    }
+  }
+
+  static byte[] read(final File f) throws IOException {
+    final byte[] b = new byte[(int) f.length()];
+    try (final FileInputStream in = new FileInputStream(f)) {
+      int offset = 0;
+      while (offset < b.length) {
+        final int read = in.read(b, offset, b.length - offset);
+        if (read == -1) {
+          throw new EOFException();
+        }
+        offset += read;
+      }
+      return b;
+    }
+  }
+
+  /**
+   * Reads the input stream, deletes fileToWriteTo if it exists and over writes
+   * it with the stream.
+   *
+   * @param readFrom
+   *          Stream to read from
+   * @param fileToWriteTo
+   *          File to write to
+   * @throws java.io.IOException
+   *           - If any of the file operations fail
+   */
+  static void cleanInstallOneFile(final InputStream readFrom, final File fileToWriteTo) throws IOException {
+    if (fileToWriteTo.exists() && !fileToWriteTo.delete()) {
+      throw new RuntimeException("Could not remove existing file " + fileToWriteTo.getName());
+    }
+    try (final OutputStream out = new FileOutputStream(fileToWriteTo)) {
+      IOUtils.copy(readFrom, out);
+    }
+  }
+
+  static void recursiveFileDelete(final File fileOrDirectory) {
+    if (fileOrDirectory.isDirectory()) {
+      for (final File child : fileOrDirectory.listFiles()) {
+        recursiveFileDelete(child);
+      }
+    }
+
+    if (fileOrDirectory.exists() && !fileOrDirectory.delete()) {
+      throw new RuntimeException("Could not delete directory " + fileOrDirectory.getAbsolutePath());
+    }
+  }
+
+  /**
+   * @param destinationDirectory
+   *          Directory files are to be extracted to
+   * @param archiveInputStream
+   *          Stream to extract
+   * @throws java.io.IOException
+   *           - If there are any file errors
+   */
+  static void extractContentFromArchive(final File destinationDirectory, final InputStream archiveInputStream)
+      throws IOException {
+    try (final TarArchiveInputStream in = new TarArchiveInputStream(new XZCompressorInputStream(archiveInputStream))) {
+      ArchiveEntry entry;
+      while ((entry = in.getNextEntry()) != null) {
+
+        final File f = new File(destinationDirectory.getCanonicalPath() + File.separator
+            + entry.getName().replace('/', File.separatorChar));
+        if (entry.isDirectory()) {
+          if (!f.exists() && !f.mkdirs()) {
+            throw new IOException("could not create directory " + f);
+          }
+        } else {
+          if (f.exists() && !f.delete()) {
+            throw new RuntimeException(
+                "Could not delete file in preparation for overwriting it. File - " + f.getAbsolutePath());
+          }
+
+          if (!f.createNewFile()) {
+            throw new RuntimeException("Could not create file " + f);
+          }
+
+          try (final OutputStream fileOutputStream = new FileOutputStream(f)) {
+            IOUtils.copy(in, fileOutputStream);
+            final int mode = ((TarArchiveEntry) entry).getMode();
+            //octal representation
+            if ((mode & 00100) > 0) {
+              f.setExecutable(true, (mode & 00001) == 0);
+            }
+          }
+        }
+      }
+    }
+  }
+}
