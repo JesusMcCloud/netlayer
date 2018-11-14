4be9490545fc111bf0d3e9b964ef624da46e2f7a
Florian Reimair <office@florianreimair.at>
2018-11-14 11:42:44 +0100

    added external hidden service demo

---
+++ tor.external/src/test/kotlin/org/berndpruenster/netlayer/tor/demo/ExternalDemoHiddenService.kt
@@ -0,0 +1,24 @@
+package org.berndpruenster.netlayer.tor.demo
+
+import org.berndpruenster.netlayer.tor.*
+import java.io.File
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.concurrent.thread
+
+
+fun main(args: Array<String>) {
+
+	Tor.default = ExternalTor(9151, File("/path/to/tor/control_auth_cookie"))
+
+	var server = HiddenServiceSocket(10025,"N/A")
+    
+	// wait for the service to be published
+	Thread.sleep(25000)
+
+	// create simple hidden service service
+	thread { BufferedReader(InputStreamReader(server.accept().getInputStream())).use { println(it.readLine()) } }
+
+	// talk to the newly created hidden service
+	ExternalTorSocket(9150, server.serviceName + ".onion", 10025).outputStream.write("Hello Tor\n".toByteArray())
+}
